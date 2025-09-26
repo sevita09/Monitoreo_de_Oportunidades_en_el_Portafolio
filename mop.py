bff15ccf3e9567ab2ed9238c4569c8b3070395ea
svita <seba.vita@gmail.com>
2025-09-26 11:38:31 -0300

    primer inicio de navbar y sidebar

--- mop.py
+++ mop.py
@@ -1,31 +1,97 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, page_container
+import dash_bootstrap_components as dbc
 from paginas.datos_pais import pagina_datos_del_pais
 from paginas.pagina_renta_fija import pagina_renta_fija
 from paginas.pagina_renta_variable import pagina_renta_variable
 
-app = Dash(__name__)
+app = Dash(__name__,
+           use_pages=True,
+           pages_folder="paginas",
+           assets_folder="assets",
+           external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME],
+           #external_stylesheets=[dbc.themes.COSMO, dbc.icons.FONT_AWESOME],
+           )
 
 # Quitar para probar en local y agregar para desplegar en Prod
-server = app.server
+#server = app.server
+
+app.title = "MOP - Monitoreo de Oportunidades en el Portafolio"
+app.config.suppress_callback_exceptions = True
+
+sidebar = html.Div([
+    dbc.Nav([
+        dbc.NavLink([
+           # html.I(className="fas fa-home me-2"),
+            html.I(className="flag-icon-ar me flag-icon"), 
+            html.Span("Datos del país")
+            ],
+            href="/datos_pais",
+            active="exact",
+        ),
+        dbc.NavLink([
+            html.I(className="fas fa-chart-simple me-2"),
+            html.Span("Market"),
+        ],
+            href="/renta_fija",
+            active="exact",
+        ),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    className="sidebar",
+)
+
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            # Lado izquierdo: logo + nombre
+            dbc.NavbarBrand(
+                [
+                    html.Img(src="https://via.placeholder.com/40", height="30px", className="me-2"),
+                    "Mi Sitio"
+                ],
+                href="/",  # link a home
+                className="d-flex align-items-center"
+            ),
+
+            # Lado derecho
+            dbc.Nav(
+                [
+                    # Botón GitHub
+                    dbc.NavItem(
+                        dbc.NavLink("GitHub", href="https://github.com/", target="_blank")
+                    ),
+
+                    # Dropdown de temas
+                    dbc.DropdownMenu(
+                        [html.I(className="fa fa-sun me-2")],
+                        children=[
+                            dbc.DropdownMenuItem("Light", [html.I(className="fa fa-sun me-2")], id="theme-light"),
+                            dbc.DropdownMenuItem("Dark", [html.I(className="fa fa-moon me-2")], id="theme-dark")
+                        ],
+                        nav=True,
+                        in_navbar=True,
+                        align_end=True,
+                    ),
+                ],
+                className="ms-auto",  # alinea a la derecha
+                navbar=True
+            ),
+        ]
+    ),
+    color="primary",
+    dark=True,
+)
 
 app.layout = html.Div([
-    dcc.Tabs(
-        id="tabs",
-        value='tab_datos_pais',
-        children=[
-          dcc.Tab(
-              label='Datos del país',
-              value='tab_datos_pais'),
-          dcc.Tab(
-              label='Renta Fija',
-              value='tab_renta_fija'),
-          dcc.Tab(
-              label='Renta Variable',
-              value='tab_renta_variable')
-    ],style={'display': 'flex', 'justify-content': 'center'}),
-    html.Div(id='pagina_datos_del_pais'),
-    html.Div(id='pagina_renta_fija'),
-    html.Div(id='pagina_renta_variable')
+    navbar,
+    html.Div([
+        sidebar,
+        page_container
+    ],
+    className="content")
 ])
 
 if __name__ == '__main__':
