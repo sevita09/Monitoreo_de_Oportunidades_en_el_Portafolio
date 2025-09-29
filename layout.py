fbe69c8e97c480a7915e9ec4f22b0b27f757b403
svita <seba.vita@gmail.com>
2025-09-29 01:35:21 -0300

    Estructura base con 3 páginas principales ya funcionando

--- layout.py
+++ layout.py
@@ -1,96 +1,92 @@
-import dash
 import dash_bootstrap_components as dbc
-from dash import Input, Output, dcc, html
+from dash import Input, Output, html, callback
+from paginas.datos_pais import pagina_datos_del_pais
+from paginas.pagina_renta_fija import pagina_renta_fija
+from paginas.pagina_renta_variable import pagina_renta_variable
 
-app = dash.Dash(
-    external_stylesheets=[dbc.themes.CERULEAN, dbc.icons.FONT_AWESOME]
-)
+def create_sidebar():
+    sidebar = html.Div([
+        dbc.Nav(
+            [
+                dbc.NavLink([
+                    html.I(className="fas fa-landmark me-2"), 
+                    html.Span("Datos macro")],
+                    href="/datos_macro",
+                    active="exact",
+                ),
+                dbc.NavLink([
+                        html.I(className="fas fa-chart-line me-2"),
+                        html.Span("Renta variable"),
+                    ],
+                    href="/renta_variable",
+                    active="exact",
+                ),
+                dbc.NavLink([
+                        html.I(className="fas fa-money-check-alt me-2"),
+                        html.Span("Renta fija"),
+                    ],
+                    href="/renta_fija",
+                    active="exact",
+                ),
+            ],
+            vertical=True,
+            pills=True
+        ),
+    ],
+    className="sidebar"
+    )
+    return sidebar
 
-sidebar = html.Div([
-    dbc.Nav(
-        [
-            dbc.NavLink([
-                html.I(className="fas fa-landmark me-2"), 
-                html.Span("Datos macro")],
-                href="/datos_macro",
+def create_navbar():
+    navbar = dbc.NavbarSimple(
+        children=[
+            dbc.NavLink(
+                [html.I(className="fab fa-github-alt me-2")],
+                href="https://github.com/sevita09/Monitoreo_de_Oportunidades_en_el_Portafolio",
                 active="exact",
             ),
-            dbc.NavLink([
-                    html.I(className="fas fa-chart-line me-2"),
-                    html.Span("Renta variable"),
+            dbc.DropdownMenu(
+                children=[
+                    dbc.DropdownMenuItem(html.I(className="fas fa-sun modo_oscuro_claro_sol", style={"color":"white"})),
+                    dbc.DropdownMenuItem(html.I(className="fas fa-moon modo_oscuro_claro_luna", style={"color":"white"}))
                 ],
-                href="/renta_variable",
-                active="exact",
-            ),
-            dbc.NavLink([
-                    html.I(className="fas fa-money-check-alt me-2"),
-                    html.Span("Renta fija"),
-                ],
-                href="/renta_fija",
-                active="exact",
+                label=html.I(className="fa fa-adjust"),
+                color="primary",
+                nav=True,
+                in_navbar=True,
             ),
         ],
-        vertical=True,
-        pills=True
-    ),
-],
-className="sidebar"
-)
+        brand=[
+            dbc.Container([
+                html.Img(src="https://i.postimg.cc/kGMK8dCc/Captura-de-pantalla-2025-09-28-a-la-s-12-56-16-a-m.png", height="40px"),
+                dbc.NavbarBrand("MOP", className="nav-brand-name")
+            ])
+        ],
+        brand_href="/",
+        color="dark",
+        dark=True,
+        sticky="top",
+        fluid=True
+    )
+    return navbar
 
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavLink(
-            [html.I(className="fab fa-github-alt me-2")],
-            href="https://github.com/sevita09/Monitoreo_de_Oportunidades_en_el_Portafolio",
-            active="exact",
-        ),
-        dbc.DropdownMenu(
-            children=[
-                dbc.DropdownMenuItem(html.I(className="fas fa-sun modo_oscuro_claro_sol", style={"color":"white"})),
-                dbc.DropdownMenuItem(html.I(className="fas fa-moon modo_oscuro_claro_luna", style={"color":"white"}))
-            ],
-            label=html.I(className="fa fa-adjust"),
-            color="primary",
-            nav=True,
-            in_navbar=True,
-        ),
-    ],
-    brand=[
-        dbc.Container([
-            html.Img(src="https://i.postimg.cc/kGMK8dCc/Captura-de-pantalla-2025-09-28-a-la-s-12-56-16-a-m.png", height="40px"),
-            dbc.NavbarBrand("MOP", className="nav-brand-name")
-        ])
-    ],
-    brand_href="/",
-    color="dark",
-    dark=True,
-    sticky="top",
-    fluid=True
-)
-
-content = html.Div(
-    id="page-content", 
-    className="content"
-)
-
-app.layout = html.Div(
-    [
-        dcc.Location(id="url"),
-        navbar,   
-        sidebar,
-        dbc.Container(id="page-content", className="pt-4"),
-    ]
-)
+def create_content():
+    return html.Div(
+        id="page-content", 
+        className="content"
+    )
 
 # set the content according to the current pathname
-@app.callback(Output("page-content", "children"), Input("url", "pathname"))
+@callback(Output("page-content", "children"), Input("url", "pathname"))
 def render_page_content(pathname):
     if pathname == "/":
         return html.P("This is the home page!")
-    elif pathname == "/calendar":
-        return html.P("This is your calendar... not much in the diary...")
-    elif pathname == "/messages":
-        return html.P("Here are all your messages")
+    elif pathname == "/datos_macro":
+        return pagina_datos_del_pais()
+    elif pathname == "/renta_variable":
+        return pagina_renta_variable()
+    elif pathname == "/renta_fija":
+        return pagina_renta_fija()
     # If the user tries to reach a different page, return a 404 message
     return html.Div(
         [
@@ -100,7 +96,3 @@ def render_page_content(pathname):
         ],
         className="p-3 bg-light rounded-3",
     )
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- mop.py
+++ mop.py
@@ -1,41 +1,53 @@
 from dash import Dash, html, dcc, page_container
 import dash_bootstrap_components as dbc
-from paginas.datos_pais import pagina_datos_del_pais
-from paginas.pagina_renta_fija import pagina_renta_fija
-from paginas.pagina_renta_variable import pagina_renta_variable
-from layout import create_navbar
+from layout import create_navbar, create_sidebar, create_content
 import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(
+    external_stylesheets=[dbc.themes.CERULEAN, dbc.icons.FONT_AWESOME]
+)
 app.title = "MOP - Monitoreo de Oportunidades en el Portafolio"
 app.config.suppress_callback_exceptions = True
 
 # Quitar para probar en local y agregar para desplegar en Prod
-#server = app.server
+server = app.server
 
 navbar = create_navbar()
+sidebar = create_sidebar()
+content = create_content()
 
-app.layout = dbc.Container([
-    navbar,
-    dcc.Tabs(
-        id="tabs",
-        value='tab_datos_pais',
-        children=[
-          dcc.Tab(
-              label='Datos del país',
-              value='tab_datos_pais'),
-          dcc.Tab(
-              label='Renta Fija',
-              value='tab_renta_fija'),
-          dcc.Tab(
-              label='Renta Variable',
-              value='tab_renta_variable')
-    ],style={'display': 'flex', 'justify-content': 'center'}),
-    html.Div(id='pagina_datos_del_pais'),
-    html.Div(id='pagina_renta_fija'),
-    html.Div(id='pagina_renta_variable')
-], fluid=True,
-class_name='px-0')
+app.layout = html.Div(
+    [
+        dcc.Location(id="url"),
+        navbar,   
+        sidebar,
+        content
+    ]
+)
+
+
+
+#app.layout = dbc.Container([
+#    navbar,
+#    dcc.Tabs(
+#        id="tabs",
+#        value='tab_datos_pais',
+#        children=[
+#          dcc.Tab(
+#              label='Datos del país',
+#              value='tab_datos_pais'),
+#          dcc.Tab(
+#              label='Renta Fija',
+#              value='tab_renta_fija'),
+#          dcc.Tab(
+#              label='Renta Variable',
+#              value='tab_renta_variable')
+#    ],style={'display': 'flex', 'justify-content': 'center'}),
+#    html.Div(id='pagina_datos_del_pais'),
+#    html.Div(id='pagina_renta_fija'),
+#    html.Div(id='pagina_renta_variable')
+#], fluid=True,
+#class_name='px-0')
 
 if __name__ == '__main__':
     app.run(debug=True)
